a3f39c6d33e0f466f4acf5c97754643a7f178650
bliff182 <michael.bither@gmail.com>
2020-02-25 09:53:20 -0500

    added api routes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,6 +5,16 @@ export default {
   getRestaurants: function(q) {
     // return axios.get("/api/yelp", { params: { q: "&location:" + q } });
     return axios.get("/api/yelp");
+  },
+  // Gets all liked Restaurants
+  getLikes: function() {
+    return axios.get("/api/restaurants");
+  },
+  // Deletes the liked restaurant
+  deleteLike: function(id) {
+    return axios.delete("/api/restaurants" + id);
+  },
+  likeRestaurant: function(restaurantData) {
+    return axios.post("/api/books", restaurantData);
   }
-  // Gets
 };
