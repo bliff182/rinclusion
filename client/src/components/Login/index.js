c51dfb4446a945f500d9df736b19eab350bfdf22
bliff182 <michael.bither@gmail.com>
2020-02-25 10:05:02 -0500

    firebase auth added to login form

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,29 +1,38 @@
 import React, { Component } from "react";
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import Container from '@material-ui/core/Container';
-
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
+import * as firebase from "firebase";
 
 // import "./style.css";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    '& > *': {
-      margin: theme.spacing(1),
-    },
+    "& > *": {
+      margin: theme.spacing(1)
+    }
   }
 }));
 
 function Form(props) {
-
   const classes = useStyles();
-  const { email, password, handleInputChange, handleFormSubmit } = props
+  const { email, password, handleInputChange, handleFormSubmit } = props;
   return (
     <div style={{ textAlign: "center" }}>
-      <form className={classes.root} autoComplete="off" style={{ textAlign: "center", padding: "50px", maxWidth: "100%", marginTop: "30px" }}>
+      <form
+        className={classes.root}
+        autoComplete="off"
+        style={{
+          textAlign: "center",
+          padding: "50px",
+          maxWidth: "100%",
+          marginTop: "30px"
+        }}
+      >
         <Container>
-          <TextField style={{ marginBottom: "20px" }}
+          <TextField
+            style={{ marginBottom: "20px" }}
             id="outlined-basic"
             variant="outlined"
             value={email}
@@ -34,7 +43,8 @@ function Form(props) {
           />
         </Container>
         <Container>
-          <TextField style={{ marginBottom: "20px" }}
+          <TextField
+            style={{ marginBottom: "20px" }}
             id="outlined-basic"
             variant="outlined"
             value={password}
@@ -44,19 +54,28 @@ function Form(props) {
             label="Password"
           />
         </Container>
-        <Button variant="contained" style={{ backgroundColor: "red" }} onClick={handleFormSubmit}>Log in</Button>
-        <p style={{textAlign:"center", fontSize:"12px", marginTop:"20px"}}>Don't have an account?</p>
-        <a style={{textAlign:"center", fontSize:"12px"}} href="/Signup">Sign up!</a>
-
+        <Button
+          variant="contained"
+          style={{ backgroundColor: "red" }}
+          onClick={handleFormSubmit}
+        >
+          Log in
+        </Button>
+        <p style={{ textAlign: "center", fontSize: "12px", marginTop: "20px" }}>
+          Don't have an account?
+        </p>
+        <a style={{ textAlign: "center", fontSize: "12px" }} href="/Signup">
+          Sign up!
+        </a>
       </form>
     </div>
-  )
+  );
 }
 
 class FormLogin extends Component {
   state = {
     email: "",
-    password: "",
+    password: ""
   };
 
   handleInputChange = event => {
@@ -76,18 +95,37 @@ class FormLogin extends Component {
     event.preventDefault();
     if (!this.state.email || !this.state.password) {
       alert("Please fill out every field");
-    } 
+    } else {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.state.email, this.state.password)
+        .catch(error => {
+          const errorCode = error.errorCode;
+          const errorMessage = error.errorMessage;
+          console.log(errorCode);
+          console.log(errorMessage);
+          this.setState({
+            email: "",
+            password: ""
+          });
+        });
+    }
 
-    this.setState({
-      email: "",
-      password: "",
-    });
+    // this.setState({
+    //   email: "",
+    //   password: ""
+    // });
   };
 
   render() {
     return (
-      <Form email={this.state.email} password={this.state.password} handleInputChange={this.handleInputChange} handleFormSubmit={this.handleFormSubmit} />
-    )
+      <Form
+        email={this.state.email}
+        password={this.state.password}
+        handleInputChange={this.handleInputChange}
+        handleFormSubmit={this.handleFormSubmit}
+      />
+    );
   }
 }
 
