90530df095893924153a3101db49d8ec7629437a
bliff182 <michael.bither@gmail.com>
2020-02-27 20:19:36 -0500

    likes appearing in account page

--- client/src/components/Liked/index.js
+++ client/src/components/Liked/index.js
@@ -3,18 +3,23 @@ import React from "react";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 
-
 function PrevLiked(props) {
   return (
     <List className="list-group search-results">
       {props.restaurants.map(result => (
-        <ListItem key={result} className="list-group-item">
-          <img alt="Restaurants" src={result} className="img-fluid" style={{ height:"100px", width:"150px"}} />
-          <button>X</button>
+        <ListItem key={result.id} className="list-group-item">
+          <img
+            alt="Restaurants"
+            src={result.image_url}
+            className="img-fluid"
+            style={{ height: "100px", width: "150px" }}
+          />
+          <button dataId={result._id}>X</button>
         </ListItem>
       ))}
     </List>
   );
+  // console.log(props.restaurants);
 }
 
 export default PrevLiked;
--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -1,57 +1,45 @@
 import React, { Component } from "react";
 import API from "../utils/API";
 import Alert from "../components/Alert";
-import Liked from "../components/Liked";
+import PrevLiked from "../components/Liked";
 import SwipeableTemporaryDrawer from "../components/Leftbar";
-import Container from '@material-ui/core/Container';
-
-
-
+import Container from "@material-ui/core/Container";
 
 class LikedRestaurants extends Component {
-    state = {
-        restaurants: []
-    };
+  state = {
+    restaurants: []
+  };
 
-    componentDidMount() {
-        this.handlegetlikedrestaurants();
-    }
-    
+  componentDidMount() {
+    this.grabLikes();
+  }
 
-    handlegetlikedrestaurants = () => {
-        // event.preventDefault();
-        API.getLikes()
-        .then(res => {
-            if (res.data.status === "error") {
-              throw new Error(res.data);
-            }
-            this.setState({ restaurants: res.data, error: "" });
-          })
-          .catch(err => this.setState({ error: err }));
-    }
+  grabLikes() {
+    API.getLikes()
+      .then(res => {
+        this.setState({
+          restaurants: res.data
+        });
+      })
+      .catch(err => console.log(err));
+  }
 
-    render() {
-        return (
-            
-            <div>
-                <Container style={{ minHeight: "80%" }}>
-                    <Alert
-                        type="danger"
-                        style={{ opacity: this.state.error ? 1 : 0, marginBottom: 10 }}
-                    >
-                        {this.state.error}
-                    </Alert>
-                    <SwipeableTemporaryDrawer
-                        handlegetlikedrestaurants={this.handlegetlikedrestaurants}
-                    />
-                    <Liked restaurants={this.state.restaurants} />
-                </Container>
-            </div>
-            
-        ) 
-    }
-    
+  render() {
+    return (
+      <div>
+        <Container style={{ minHeight: "80%" }}>
+          <Alert
+            type="danger"
+            style={{ opacity: this.state.error ? 1 : 0, marginBottom: 10 }}
+          >
+            {this.state.error}
+          </Alert>
+          <SwipeableTemporaryDrawer grabLikes={this.grabLikes} />
+          <PrevLiked restaurants={this.state.restaurants} />
+        </Container>
+      </div>
+    );
+  }
 }
-
 
 export default LikedRestaurants;
