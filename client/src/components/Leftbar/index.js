38bf4e64d8783088a8bd7eaf4a90c9ffc5ca0d59
Marc Dycus <marcdycus@yahoo.com>
2020-02-29 10:14:15 -0500

    conflict changes

--- client/src/components/Leftbar/index.js
+++ client/src/components/Leftbar/index.js
@@ -1,20 +1,10 @@
-<<<<<<< HEAD
 import React, { Component } from "react";
-=======
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
-import Button from "@material-ui/core/Button";
-import MenuIcon from "@material-ui/icons/Menu";
-import Divider from "@material-ui/core/Divider";
->>>>>>> bb8e2df3a69ce0c7c09c33144ddf60bdc16c4659
 import API from "../../utils/API";
 import Alert from "../Alert";
 import PrevLiked from "../Liked";
 import SwipeableTemporaryDrawer from "../Leftbar";
 import Container from "@material-ui/core/Container";
 
-<<<<<<< HEAD
 class LikedRestaurants extends Component {
   state = {
     restaurants: []
@@ -50,58 +40,6 @@ class LikedRestaurants extends Component {
       </div>
     );
   }
-=======
-const useStyles = makeStyles({
-  list: {
-    width: 250
-  },
-  fullList: {
-    width: "auto"
-  }
-});
-
-function SwipeableTemporaryDrawer() {
-  const classes = useStyles();
-  const [state, setState] = React.useState({
-    left: false
-  });
-
-  const toggleDrawer = (side, open) => event => {
-    if (
-      event &&
-      event.type === "keydown" &&
-      (event.key === "Tab" || event.key === "Shift")
-    ) {
-      return;
-    }
-    setState({ ...state, [side]: open });
-  };
-
-  const sideList = side => (
-    <div
-      className={classes.list}
-      role="presentation"
-      onClick={toggleDrawer(side, false)}
-      onKeyDown={toggleDrawer(side, false)}
-    >
-      <PrevLiked />
-    </div>
-  );
-
-  return (
-    <div>
-      <MenuIcon onClick={toggleDrawer("left", true)} href="/Liked"></MenuIcon>
-      <SwipeableDrawer
-        open={state.left}
-        onClose={toggleDrawer("left", false)}
-        onOpen={toggleDrawer("left", true)}
-        // handlegetlikedrestaurants={this.handlegetlikedrestaurants}
-      >
-        {sideList("left")}
-      </SwipeableDrawer>
-    </div>
-  );
->>>>>>> bb8e2df3a69ce0c7c09c33144ddf60bdc16c4659
 }
 
 export default LikedRestaurants;
